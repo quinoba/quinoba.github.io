c29c183902b6195c748d219a4cdefd941be1e77b
quinoba <87843175+quinoba@users.noreply.github.com>
2025-07-07 19:53:31 +0000

    Deploying to gh-pages from @ quinoba/quinoba.github.io@d031549a5b6c64fbda00acdb97f71dc638bac6c7 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -39,7 +39,7 @@ ninja.data = [{
           },
         },{id: "nav-cv",
           title: "cv",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+          description: "You can view my CV directly below or download it as a PDF.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
