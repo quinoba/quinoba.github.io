7646ce7548ad705029028eaf58d8278e2bb279ea
quinoba <87843175+quinoba@users.noreply.github.com>
2025-10-18 19:59:20 +0000

    Deploying to gh-pages from @ quinoba/quinoba.github.io@498785fe8cce3b20516b2e08ec6b2741d8529ee1 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -64,9 +64,9 @@ ninja.data = [{
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
           description: "",
-          section: "News",},{id: "projects-project-1",
-          title: 'project 1',
-          description: "with background image",
+          section: "News",},{id: "projects-sí-al-desarme-sí-a-la-paz",
+          title: 'Sí al Desarme, Sí a la Paz',
+          description: "Daily location of gun buyback stations",
           section: "Projects",handler: () => {
               window.location.href = "/projects/si_al_desarme/";
             },},{
