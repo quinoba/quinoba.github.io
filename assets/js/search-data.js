b4f2138b2fe5fcbf02afc875aeade8e46297761f
quinoba <87843175+quinoba@users.noreply.github.com>
2026-04-26 23:00:00 +0000

    Deploying to gh-pages from @ quinoba/quinoba.github.io@6e5cc0b9a8776e0e4f66f9ca352a44e632fab12d 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -21,7 +21,7 @@ ninja.data = [{
           description: "",
           section: "Navigation",
           handler: () => {
-            window.location.href = "/publications/";
+            window.location.href = "/research/";
           },
         },{id: "nav-teaching",
           title: "Teaching",
